139d31657ba38d672399c077a72b445c39d74326
nekomeo <linh.tu545@gmail.com>
2017-10-15 10:47:56 -0700

    Minor adjustment

--- WatchApp/ViewController.swift
+++ WatchApp/ViewController.swift
@@ -15,13 +15,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        dateLabel.text = getDateString()
+        
+        // Timer start
+        let timer = Timer.scheduledTimer(timeInterval: 1.0,
+                                         target: self,
+                                         selector: #selector(updateTime),
+                                         userInfo: nil,
+                                         repeats: true)
+        timer.fire()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func getDateString() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMMM, dd, yyyy"
+        return formatter.string(from: Date())
+    }
+    
+    @objc func updateTime() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss"
+        
+        // Set current time
+        timeLabel.text = formatter.string(from: Date())
+    }
 }
 
